7911e4de85c0a3ba8378a4f7bd8f53e6deac675a
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-04-26 00:38:16 -0300

    Funcionalidades separadas em funções. Só falta desenvolver o input da data de aniversário.

--- Birthday Counter/js/birthdayCounterJS.js
+++ Birthday Counter/js/birthdayCounterJS.js
@@ -1,25 +1,36 @@
-
-function showCurrentDay(){
+function getCurrentDay(){
     currentDate = new Date();
     day = currentDate.getDate();
     month = currentDate.getMonth() + 1;
     year = currentDate.getFullYear();
-    
-    todayIs = new Date(`${month}/${day}/${year}`)
-    const date2 = new Date('8/28/2020');
-    const diffTime = Math.abs(date2 - todayIs);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-    console.log(diffDays);
-    
+
+    return day, month, year;
+}
+
+function showCurrentDay(day, month, year){
+  
     day < 10 ? day = "0" + day : day = day;
     month < 10 ? month = "0" + month : month = month;
 
     currentDay = document.getElementById("currentDay");
-    currentDay.textContent = (`${day}/${month}/${year}`)
-
-
-   
-    
+    currentDay.textContent = (`${day}/${month}/${year}`);   
 }
 
-showCurrentDay()
+function getDaysForBirthday(day, month, year){
+    todayIs = new Date(`${month}/${day}/${year}`)
+    const date2 = new Date('8/28/2020');
+    const diffTime = Math.abs(date2 - todayIs);
+    diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    return (diffDays);
+}
+
+function showDaysForBirthday(diffDays){
+    daysForBirthday = document.getElementById("daysForBirthday");
+    daysForBirthday.textContent = `There's ${diffDays} days for your birthday!!`;
+}
+
+getCurrentDay();
+showCurrentDay(day, month, year);
+getDaysForBirthday(day, month, year);
+showDaysForBirthday(diffDays)
+
