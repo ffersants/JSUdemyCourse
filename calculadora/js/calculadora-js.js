d4e7b888a07990f2ea6ca2e6e0d0229a68974781
Fernando <fersants.nando.ferreira@gmail.com>
2020-05-09 00:16:10 -0300

    Agora sim quase tudo pronto... Fazer a página responsiva?

--- calculadora/js/calculadora-js.js
+++ calculadora/js/calculadora-js.js
@@ -1,20 +1,32 @@
 visor = document.getElementById("console");
-
+i = 0;
 function somethingClicked(tipo, valor){
     if (tipo === 'numero'){
         visor.value += valor;
+        // console.log(visor.value.length);
+        if (i != 0){
+            visor.value = valor;
+            i = 0;
+        }
     }
     else if(tipo === 'operador'){
         visor.value += valor;
+        if(i = 1){
+            i = 0;
+        }
     }
     else if(tipo === 'igual'){
-        total = eval(visor.value)
+        total = eval(visor.value);
         visor.value = total;
+        i=1;
     }
 }
 
+
 function del(){
-    inputLength = visor.value.length;
-    inputLessOne = inputLength - 1;
-    visor.value = visor.value.substring(inputLength - inputLessOne)
+    visorValue = visor.value;
+    consoLength = visorValue.length;
+    consoLengthLessOne = consoLength - 1;
+    visorValue = visorValue.slice(0, consoLengthLessOne)
+    visor.value = visorValue;
 }
