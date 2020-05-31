9709b47af82ab0c9986f25c23ed0ea83154e36a8
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-05-31 20:15:34 -0300

    Cronometro funcionando junto com a posição randomica.

--- gameProject/js/game-script.js
+++ gameProject/js/game-script.js
@@ -1,11 +1,8 @@
 
 //gets random x & y point to show the mosca
     mosca = document.getElementById("mosca");
-    
-    
-    main();
 
-    function main(){
+function main(){
         height = window.innerHeight;
         width = window.innerWidth;
 
@@ -15,13 +12,15 @@
         mosca.style.right = randomWidth + "px";
         mosca.style.bottom = (randomHeight) + "px";
 
-        mosca.className = 'name' + Math.floor(Math.random()*2)
-    }
-
-
-document.onresize = function(){
-    main();
+        mosca.className = 'name' + Math.floor(Math.random()*2);
 }
+
+
+   
+
+// document.onresize = function(){
+//     cronometro();
+// }
 
 mosca.onclick = function(){
     main()
@@ -30,20 +29,28 @@ mosca.onclick = function(){
 
 //timer section
 
-segundos = 100
- function cronometro(){
+segundos = 20;
+contador = segundos;
+
+function cronometro(){
+    
     setInterval(() => {
         if (segundos < 0){
             clearInterval(cronometro);
-        } else{
+        } 
+        
+        else{         
             tempo = document.getElementById("timer");
-            tempo.innerHTML = `Tempo restante ${segundos} `
+            tempo.innerHTML = `Tempo restante ${segundos}`
             segundos--
+            
+            if (contador - segundos === 3){
+                contador = segundos;
+                main()
+            }
+            
         }
-    }, 1000);
- }
+    } , 1000);
+}
 
- cronometro()
-
-
-
+cronometro()
