70759afbcad140bf73291b5ac7b892262bf0df75
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-07-19 18:20:29 -0300

    Projeto concluído! Refatorar código depois de já ter utilizado o sistema por um tempo sem encontrar erros, e também, depois de ter concluído o projeto do trabalho.

--- Projeto Orcamento Pessoal/js/app.js
+++ Projeto Orcamento Pessoal/js/app.js
@@ -15,7 +15,7 @@ document.addEventListener("keydown", () => {
             this.tipo = tipo;
             this.descricao = descricao
             this.valor = valor;
-            this.data = `${this.dia}/${this.mes}/${this.ano}`
+            // this.data = `${this.dia}/${this.mes}/${this.ano}`
         }
 
         validarDados() {
@@ -77,6 +77,51 @@ document.addEventListener("keydown", () => {
             
             return despesas
         }
+
+        pesquisar(despesaPesquisada) {
+            let todasDespesas = this.recuperarTodosRegistros();
+            let despesasFiltradas = Array();
+
+            //despesas filtradas recebe todas as despesas cadastradas, e atualiza a si mesma,
+            //a cada vez que entra em uma condição
+            despesasFiltradas = todasDespesas;
+   
+            //filtra ano
+            if (despesaPesquisada.ano != ''){
+                despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesaPesquisada.ano);            
+            }   
+
+            //filtra mês
+            if ( despesaPesquisada.mes != '' ){
+                despesasFiltradas = despesasFiltradas.filter( d => d.mes == despesaPesquisada.mes );
+            }
+
+            //filtra tipo
+            console.log(despesaPesquisada)
+            if ( despesaPesquisada.tipo != '' ){
+                despesasFiltradas = despesasFiltradas.filter( f => f.tipo == despesaPesquisada.tipo );
+            }
+
+            //filtra dia
+            if ( despesaPesquisada.dia != '' ){
+                despesasFiltradas = despesasFiltradas.filter( d => d.dia == despesaPesquisada.dia );
+            }
+
+            //filtra descrição
+            if ( despesaPesquisada.descricao != '' ){
+                despesasFiltradas = despesasFiltradas.filter( d => d.descricao == despesaPesquisada.descricao );
+            }
+
+            //filtra valor
+            if ( despesaPesquisada.valor != '' ){
+                despesasFiltradas = despesasFiltradas.filter( d => d.valor == despesaPesquisada.valor );
+            }
+            
+
+            return despesasFiltradas
+
+        }
+        
     }//ends class BD
     let bd = new Bd()
  
@@ -98,8 +143,8 @@ function cadastrarDespesa() {
 
     let despesa = new Despesa(
         ano.value,
-        dia.value,
         mes.value,
+        dia.value,
         tipoInText,
         descricao.value,
         valor.value
@@ -167,49 +212,105 @@ function carregaListaDespesas(){
     })
 }
 
+function pesquisar(){
 
+    let descricao = document.getElementById("descricao");
+    let valor = document.getElementById("valor");
+    let ano = document.getElementById("ano");
+    let mes = document.getElementById("mes");
+    let dia = document.getElementById("dia");
+    let tipo = document.getElementById("tipo");
+    let tipoInText = tipo.options[tipo.selectedIndex].text;
+    tipoInText === "Tipo" ? tipoInText = '' : tipoInText = tipoInText;
 
-function pesquisar () {
-    //DELETA REGISTROS ANTES CARREGADOS
-    //<tbody id="lista-despesas">
-    var listaDespesas = document.getElementById("lista-despesas");
-  
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipoInText,
+        descricao.value,
+        valor.value
+    )
+
+    var despesasFiltradas = bd.pesquisar(despesa);
+        
+
+        
     //pega o número total de linhas no corpo da tabela
     //CADA LINHA É UM REGISTRO
+    let listaDespesas = document.getElementById("lista-despesas");
     todasTR = listaDespesas.childNodes;
-    todasTRLength = todasTR.length
-
-    for ( contador = todasTRLength - 1; contador != 0; contador--){
+/* dentro de node list nós temos vários itens "tr", que correspondem ao número de registros
+em localstorage que estão apresentados na tabela. No entanto, se temos 5 "tr's" o console.log de todasTR,
+que apresenta o números de elementos filhos o elemento tbody com id "lista-despesas", veremos que temos
+6 elementos filhos, sendo 5 "tr's" e um elemento "text" que fica no índice 0. 
+Este elemento "text", se for apagado, causa um erro no console e faz o interpretador JS sair do código,
+sendo assim, o parâmetro a ser utilizado para apagar as linhas, está uma unidade abaixo.
+Então o loop para quando o parâmetro contador for igual a zero, sendo que em todasTR ainda teremos
+uma unidade acima que corresponde ao index de "text"
+*/
+    for ( contador = todasTR.length - 1; contador != 0; contador--){
             listaDespesas.deleteRow(todasTR)//deleta node por node abaixo de lista-despesas
-            console.log(todasTR.length)
     }
 
+    despesasFiltradas.forEach( despesa => {
+           newLine = listaDespesas.insertRow();
 
-        let tipoInText = tipo.options[tipo.selectedIndex].text;
-        let todosRegistros = localStorage.getItem('id');
-        
-        
-        for( let item = 1; item <= todosRegistros; item++){
-            //console.log(localStorage.getItem(item));
-            let registro = JSON.parse(localStorage.getItem(item));
-            console.log(registro)
-            if (registro.tipo === tipoInText){
+           //data = formatar data em DD/MM/AAAA
+           despesa.dia < 10 ? despesa.dia = '0' + despesa.dia : false 
+           despesa.mes < 10 ? despesa.mes = '0' + despesa.mes : false 
+           data = `${despesa.dia}/${despesa.mes}/${despesa.ano}`     
+           
+           newLine.insertCell(0).textContent = data;
+           newLine.insertCell(1).textContent = despesa.tipo;
+           newLine.insertCell(2).textContent = despesa.descricao;
+           newLine.insertCell(3).textContent = despesa.valor;
+        }    
+    )
     
-                newLine = listaDespesas.insertRow();
-                
-                registro.dia < 10 ? registro.dia = '0' + registro.dia : false;
-                registro.mes < 10 ? registro.mes = '0' + registro.mes : false;
-    
-                data = `${registro.dia}/${registro.mes}/${registro.ano}`
-    
-                
-                newLine.insertCell(0).textContent = data;
-                newLine.insertCell(1).textContent = registro.tipo;
-                newLine.insertCell(2).textContent = registro.descricao;
-                newLine.insertCell(3).textContent = registro.valor;
-            }
-        }
 }
+
+// function pesquisar () {
+//     //DELETA REGISTROS ANTES CARREGADOS
+//     //<tbody id="lista-despesas">
+//     var listaDespesas = document.getElementById("lista-despesas");
+  
+//     //pega o número total de linhas no corpo da tabela
+//     //CADA LINHA É UM REGISTRO
+//     todasTR = listaDespesas.childNodes;
+//     todasTRLength = todasTR.length
+
+//     for ( contador = todasTRLength - 1; contador != 0; contador--){
+//             listaDespesas.deleteRow(todasTR)//deleta node por node abaixo de lista-despesas
+//             console.log(todasTR.length)
+//     }
+
+
+//         let tipoInText = tipo.options[tipo.selectedIndex].text;
+//         let todosRegistros = localStorage.getItem('id');
+        
+        
+//         for( let item = 1; item <= todosRegistros; item++){
+//             //console.log(localStorage.getItem(item));
+//             let registro = JSON.parse(localStorage.getItem(item));
+//             console.log(registro)
+//             if (registro.tipo === tipoInText){
+    
+//                 newLine = listaDespesas.insertRow();
+                
+//                 registro.dia < 10 ? registro.dia = '0' + registro.dia : false;
+//                 registro.mes < 10 ? registro.mes = '0' + registro.mes : false;
+    
+//                 data = `${registro.dia}/${registro.mes}/${registro.ano}`
+    
+                
+//                 newLine.insertCell(0).textContent = data;
+//                 newLine.insertCell(1).textContent = registro.tipo;
+//                 newLine.insertCell(2).textContent = registro.descricao;
+//                 newLine.insertCell(3).textContent = registro.valor;
+//             }
+//         }
+// }
     
   
 
